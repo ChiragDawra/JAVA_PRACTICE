925299468af88c20c1ca8bbb946c3d267cbca103
Your GitHub Username <chiragdawra46@gmail.com>
2025-12-21 23:02:13 +0530

    Day3

--- DECEMBER(2025)/DAY3.java
+++ DECEMBER(2025)/DAY3.java
@@ -1,10 +1,29 @@
 //21-12-2025
 //q-215 
-import java.util.*;
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+// import java.util.*;
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         return nums[nums.length-k];
         
+//     }
+// }
+//que 34 
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int i =0;
+        int j=nums.length-1;
+        while(i<=j){
+            if(target==nums[i] && target==nums[j]){
+                return new int[] {i,j};
+            }
+            if(nums[i]<target){
+                i++;
+            }
+            if(nums[j]>target){
+                j--;
+            }
+        }
+        return new int[] {-1,-1};
     }
 }
