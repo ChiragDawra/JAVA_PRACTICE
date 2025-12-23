c2b074c3a50b80703e4fbccb073878bb49c0d450
Your GitHub Username <chiragdawra46@gmail.com>
2025-12-23 12:11:34 +0530

    Minimum Enery Required for selection Sort

---
+++ Class/SelectionSort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class SelectionSortEnergy {
+    public static int energyRequired(int[] arr, int n) {
+        int energy = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+
+                energy += 3; 
+            }
+        }
+        return energy;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(energyRequired(arr, n));
+    }
+}
