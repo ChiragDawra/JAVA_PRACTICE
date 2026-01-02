3d88d58c52ae99cadb975c5f866d00ca89e507f7
Your GitHub Username <chiragdawra46@gmail.com>
2026-01-02 13:26:50 +0530

    DAY 7.java

--- DECEMBER(2025)/DAY7.java
+++ DECEMBER(2025)/DAY7.java
@@ -1,15 +1,31 @@
 //que no 1295
+// class Solution {
+//     public int findNumbers(int[] nums) {
+//         int countNum = 0;
+//         for (int i=0;i<nums.length;i++){
+//             int countDigit=0;
+//             while(nums[i]>0){
+//                 countDigit++;
+//                 nums[i] /=10;
+//             }
+//             if(countDigit%2==0) countNum ++;
+//         }
+//         return countNum;
+//     }
+// }
+//que no 1672
 class Solution {
-    public int findNumbers(int[] nums) {
-        int countNum = 0;
-        for (int i=0;i<nums.length;i++){
-            int countDigit=0;
-            while(nums[i]>0){
-                countDigit++;
-                nums[i] /=10;
+    public int maximumWealth(int[][] accounts) {
+        int row = accounts.length;
+        int column = accounts[0].length;
+        int sum = 0;
+        for(int i=0;i<row;i++){
+            int temp = 0;
+            for(int j=0;j<column;j++){
+                temp +=accounts[i][j];
             }
-            if(countDigit%2==0) countNum ++;
+            if(temp>sum) sum = temp;
         }
-        return countNum;
+        return sum ;
     }
 }
