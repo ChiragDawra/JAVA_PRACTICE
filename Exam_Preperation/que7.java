d55c9260e78a1ae153d24eef1f83d675101f5616
Your GitHub Username <chiragdawra46@gmail.com>
2026-01-05 22:39:41 +0530

    que7 done

---
+++ Exam_Preperation/que7.java
@@ -0,0 +1,22 @@
+class que7 {
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->  {
+            try (java.io.FileWriter fw = new java.io.         FileWriter("display_runtime.txt")) {
+                fw.write("0");
+            } catch (Exception e) {
+            }
+        }));
+    }
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for (int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
